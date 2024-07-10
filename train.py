b86235682e7202e26b7bbee2e5cb76146895fb0a
IlikeBB <ru0354m3@gmail.com>
2024-07-10 13:22:33 +0800

    update other vit module(CLIP model)

--- train.py
+++ train.py
@@ -6,9 +6,10 @@ os.environ["TOKENIZERS_PARALLELISM"] = "true"
 from main import Main__
 
 model_config = {'amp':True, 'use_distributed':False,'accum_grad_iters':1,
+                'batch_size':5,
                 'chat_template': True, 'end_sym': '\n', 'prompt_template': "[INST] {} [/INST]",
                 'max_txt_len': 1024, 'max_context_len': 3500,
-                'ouput_dir': './exper01_llama3',
+                'ouput_dir': './llama3_vit_L-clip336',
                 'stage_ckpt': '/ssd3/chih/LLM/MiniGPT-4-ckpt/checkpoint_stage3.pth', 
                 'vis_root_train': './dataset/minigpt_casing_train/coco/image/train',
                 'ann_paths_train': ['./dataset/minigpt_casing_train/coco_caption/defe_ready_anno.json'],
@@ -24,7 +25,10 @@ llm_config = {'llama_model':'/ssd3/chih/LLM/Meta-Llama-3-8B-Instruct', 'low_reso
 # '/ssd3/chih/LLM/Meta-Llama-3-8B-Instruct'
 
 
-vit_config = {'model_name':'eva_clip_g', 'image_size': 448,  'drop_path_rate': 0, 'use_grad_checkpoint': True, 'vit_precision': 'fp16', 'freeze_vit': True, }
+vit_config = {'model_name':'clip_large_336', #eva_clip_g, clip_large_336
+              'model_path':"../../VITModel/clip-vit-large-patch14-336",
+              'image_size': 336,  #bilp2 = 448, clip = 224 or 336
+              'drop_path_rate': 0, 'use_grad_checkpoint': True, 'vit_precision': 'fp16', 'freeze_vit': True, }
 
 lr_config = {'init_lr': 1e-5, 'beta2':0.999,'min_lr': 1e-6, 'decay_rate': None, 'weight_decay':0.05,
                 'warmup_start_lr': 1e-6, 'warmup_steps': 1000, 'iters_per_epoch': 1000}
